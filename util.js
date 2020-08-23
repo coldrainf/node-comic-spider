bc571ef02ad5532cac39472d29a1096212019967
coldrainf <coldrainf@163.com>
2020-08-23 20:36:55 +0800

    init

--- util.js
+++ util.js
@@ -3,7 +3,6 @@ const crypto = require('crypto')
 
 const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
 
-
 /**
  * https://www.cnblogs.com/vipstone/p/5514886.html
  * 作者 王磊
